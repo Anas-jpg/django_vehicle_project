709431193db525611bdb6e2c6f9c9f1f312712c2
anas-jpg <muhammad.anas159@gmail.com>
2024-09-24 12:50:46 +0500

    urls have been configured and admin panel code added

--- vehicle_app/admin.py
+++ vehicle_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Vehicle, Car, Bike
 
-# Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'brand', 'vehicle_type', 'color', 'year']
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['id', 'brand', 'vehicle_type', 'color', 'year', 'num_doors']
+
+@admin.register(Bike)
+class BikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'brand', 'vehicle_type', 'color', 'year', 'has_gear']

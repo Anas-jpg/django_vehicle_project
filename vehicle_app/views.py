15af8ee1141fbc226fe1011257da17a2879bada5
anas-jpg <muhammad.anas159@gmail.com>
2024-09-24 12:44:25 +0500

    Views have been added

--- vehicle_app/views.py
+++ vehicle_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vehicle, Car, Bike
+from .serializers import VehicleSerializer, CarSerializer, BikeSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BikeViewSet(viewsets.ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
+    permission_classes = [IsAuthenticated]
